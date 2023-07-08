f840746a1fb2c9c05a6921eb637d037f620ceeab
Hamireza Hashemi <IT-User1@Namagahi.co>
2023-07-08 09:09:03 +0330

    allowed origins changed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,15 +1,12 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
+    'http://portal.namaagahi.com',
     'http://localhost:3500',
-    'http://localhost:3000',
-    'http://localhost:3001',
     'http://192.168.68.35:3000',
-    'http://192.168.68.35:3001',
-    'http://192.168.68.36',
+    'http://localhost:3001',
+    'http://localhost:3000',
+    'http://192.168.68.21:3000',
     'http://it-pc1.namagahi.co:3500',
     'http://it-pc1.namagahi.co:3000',
-    'http://it-pc2.namagahi.co:3500'
 ]
 
 module.exports = allowedOrigins
