905b9245207ecce674f541fd5896e4b7388823fa
Hamireza Hashemi <IT-User1@Namagahi.co>
2023-11-04 12:08:15 +0330

    update expired structure disabled temporarily

--- controllers/structureController.js
+++ controllers/structureController.js
@@ -3,22 +3,22 @@ const asyncHandler = require('express-async-handler')
 const User = require('../model/User')
 const { updateExpiredStructures } = require('../middleware/updateExpiredStructures')
 
-// @desc Get all structures 
+// @desc Get all structures
 // @route GET /structures
 // @access Private
 const getAllStructures = asyncHandler(async (req, res) => {
 
-    await updateExpiredStructures()
+    // await updateExpiredStructures()
 
     const structures = await Structure.find().lean()
-    if (!structures?.length) 
+    if (!structures?.length)
         return res.status(400).json({ message: 'BAD REQUEST : No structures found' })
 
     const structuresWithUser = await Promise.all(structures.map(async (structure) => {
         const user = await User.findById(structure.userId).lean().exec()
         return { ...structure, username: user.username }
     }))
-    
+
     res.json(structuresWithUser)
 })
 
@@ -33,17 +33,17 @@ const createNewStructure = asyncHandler(async (req, res) => {
         location
     } = req.body
 
-    if (!userId || !name || !location ) 
+    if (!userId || !name || !location )
         return res.status(400).json({ message: 'BAD REQUEST : All fields are required' })
-    
+
     const duplicate = await Structure.findOne({ name }).lean().exec()
-    if (duplicate) 
+    if (duplicate)
         return res.status(409).json({ message: 'CONFLICT :Duplicate structure name' })
 
     const structure = await Structure.create({ userId, name, location })
-    if (structure) 
+    if (structure)
         return res.status(201).json({ message: `CREATED: Structure ${req.body.name} created successfully!` })
-    else 
+    else
         return res.status(400).json({ message: 'BAD REQUEST : Invalid box data received' })
 })
 
@@ -61,15 +61,15 @@ const updateStructure = asyncHandler(async (req, res) => {
         isChosen,
         parent
     } = req.body
-    
-    if (!id || !userId || !name || !location ) 
+
+    if (!id || !userId || !name || !location )
         return res.status(400).json({ message: 'BAD REQUEST : All fields are required' })
 
     const structure = await Structure.findById(id).exec()
     if(!structure) res.status(400).json({ message: 'BAD REQUEST : Structure not found' })
 
     const duplicate = await Structure.findOne({ name }).lean().exec()
-    if(duplicate && duplicate?._id.toString() !== id) 
+    if(duplicate && duplicate?._id.toString() !== id)
         return res.status(409).json({ message: 'CONFLICT : Duplicate structure name!' })
 
     // Update and store user
@@ -91,11 +91,11 @@ const updateStructure = asyncHandler(async (req, res) => {
 const deleteStructure = asyncHandler(async (req, res) => {
 
     const { id } = req.body
-    if (!id) 
+    if (!id)
         return res.status(400).json({ message: 'Structure ID required' })
-    
+
     const structure = await Structure.findById(id).exec()
-    if (!structure) 
+    if (!structure)
         return res.status(400).json({ message: 'Structure not found' })
 
     const result = await structure.deleteOne()
