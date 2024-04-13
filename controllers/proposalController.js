fbc1985ef5025b1ffdacd89aa4521dd3bf4996fd
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-04-13 15:53:23 +0330

    refactor: createProposal in controler

--- controllers/proposalController.js
+++ controllers/proposalController.js
@@ -1,52 +1,31 @@
-const Proposal = require('../model/Proposal')
-const asyncHandler = require('express-async-handler')
-const User = require('../model/User')
-const { io, connectedUsers } = require('../config/io')
+const Proposal = require("../model/Proposal");
+const asyncHandler = require("express-async-handler");
+const User = require("../model/User");
+const { io, connectedUsers } = require("../config/io");
 
 // @desc Get all proposals
 // @route GET /proposal
 // @access Private
 const getAllProposals = asyncHandler(async (req, res) => {
-
-  const proposals = await Proposal.find().lean()
+  const proposals = await Proposal.find().lean();
   if (!proposals?.length)
-    return res.status(400).json({ message: 'BAD REQUEST: No proposals found' })
+    return res.status(400).json({ message: "BAD REQUEST: No proposals found" });
 
   const proposalWithUser = await Promise.all(
     proposals.map(async (proposal) => {
-      const user = await User.findById(proposal.userId).lean().exec()
-      return { ...proposal, username: user.username }
+      const user = await User.findById(proposal.userId).lean().exec();
+      return { ...proposal, username: user.username };
     })
-  )
+  );
 
-  res.json(proposalWithUser)
-})
+  res.json(proposalWithUser);
+});
 
 // @desc Create a new proposal
 // @route POST /proposal
 // @access Private
 const createProposal = asyncHandler(async (req, res) => {
-
-  const { userId, subject, initialCustomerId, startDate, endDate, priority, status, type, description, assignedUsers } = req.body
-
-  // Create the proposal
- try {
-
-  if (!subject || !startDate || !endDate || !priority || !type || !assignedUsers || !description) {
-    return res.status(400).json({
-      success: false,
-      message: 'Bad Request: Required fields are missing.'
-    })
-  }
-
-  const duplicate = await Proposal.findOne({ subject }).lean().exec()
-  if (duplicate)
-    return res.status(409).json({
-      success: false,
-      message: 'CONFLICT :Duplicate proposal subject'
-    })
-
-  const proposal = new Proposal({
+  const {
     userId,
     subject,
     initialCustomerId,
@@ -57,31 +36,67 @@ const createProposal = asyncHandler(async (req, res) => {
     type,
     description,
     assignedUsers,
-  })
+  } = req.body;
 
-  const savedProposal = await proposal.save()
-  const proposalAssignedUsers = savedProposal.assignedUsers
-
-  res.status(200).json({
-    success: true,
-    message: `CREATED: Proposal ${req.body.subject} created successfully!`
-  })
-
-  proposalAssignedUsers.forEach((userId) => {
-    const userSocket = connectedUsers[userId]
-    if (userSocket) {
-      userSocket.emit('newProposal', { proposal: savedProposal })
+  // Create the proposal
+  try {
+    if (
+      !subject ||
+      !startDate ||
+      !endDate ||
+      !priority ||
+      !type ||
+      !assignedUsers ||
+      !description
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "Bad Request: Required fields are missing.",
+      });
     }
-  })
 
- } catch (error) {
-  console.log("PROPOSAL/POST ERR", error)
-  res.status(500).json({
-    success: false,
-    message: 'Internal Server Error: Failed to create a proposal.'
-  })
- }
-})
+    const duplicate = await Proposal.findOne({ subject }).lean().exec();
+    if (duplicate)
+      return res.status(409).json({
+        success: false,
+        message: "CONFLICT :Duplicate proposal subject",
+      });
+
+    const proposal = new Proposal({
+      userId,
+      subject,
+      initialCustomerId,
+      startDate,
+      endDate,
+      priority,
+      status,
+      type,
+      description,
+      assignedUsers,
+    });
+
+    const savedProposal = await proposal.save();
+    const proposalAssignedUsers = savedProposal.assignedUsers;
+
+    proposalAssignedUsers.forEach((userId) => {
+      const userSocket = connectedUsers[userId];
+      if (userSocket) {
+        userSocket.emit("newProposal", { proposal: savedProposal });
+      }
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `CREATED: Proposal ${req.body.subject} created successfully!`,
+    });
+  } catch (error) {
+    console.log("PROPOSAL/POST ERR", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error: Failed to create a proposal.",
+    });
+  }
+});
 
 // @desc Update a proposal
 // @route PUT /proposal/:id
@@ -119,31 +134,30 @@ const createProposal = asyncHandler(async (req, res) => {
 // @route DELETE /proposals
 // @access Private
 const deleteProposal = asyncHandler(async (req, res) => {
-
-  const { id } = req.body
+  const { id } = req.body;
 
   if (!id)
-      return res.status(400).json({
-          success: false,
-          message: 'Proposal ID required'
-      })
+    return res.status(400).json({
+      success: false,
+      message: "Proposal ID required",
+    });
 
-  const proposal = await Proposal.findByIdAndDelete(id)
+  const proposal = await Proposal.findByIdAndDelete(id);
 
   if (!proposal)
-      return res.status(400).json({
-          success: false,
-          message: 'Proposal not found'
-      })
+    return res.status(400).json({
+      success: false,
+      message: "Proposal not found",
+    });
 
   res.status(200).json({
-      success: true,
-      message: `DELETED: Proposal ${proposal.subject} deleted successfully!`
-  })
-})
+    success: true,
+    message: `DELETED: Proposal ${proposal.subject} deleted successfully!`,
+  });
+});
 
 module.exports = {
   getAllProposals,
   createProposal,
-  deleteProposal
-}
+  deleteProposal,
+};
