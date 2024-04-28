24ea0411b6632fce707201661696f90c081c7165
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-04-28 15:10:57 +0330

    refactor : add default value to count

--- controllers/projectCodeController.js
+++ controllers/projectCodeController.js
@@ -1,39 +1,33 @@
-const ProjectCode = require('../model/ProjectCode')
-const asyncHandler = require('express-async-handler')
-const User = require('../model/User')
+const ProjectCode = require("../model/ProjectCode");
+const asyncHandler = require("express-async-handler");
+const User = require("../model/User");
 
 // @desc Get all projectCodes
 // @route GET /projectCodes
 // @access Private
 const getAllProjectCodes = asyncHandler(async (req, res) => {
+  const projectCodes = await ProjectCode.find().lean();
+  if (!projectCodes?.length)
+    return res
+      .status(400)
+      .json({ message: "BAD REQUEST : No project codes found" });
 
-    const projectCodes = await ProjectCode.find().lean()
-    if (!projectCodes?.length)
-        return res.status(400).json({ message: 'BAD REQUEST : No project codes found' })
+  const projectCodesWithUser = await Promise.all(
+    projectCodes.map(async (projectCode) => {
+      const user = await User.findById(projectCode.userId).lean().exec();
+      return { ...projectCode, username: user.username };
+    })
+  );
 
-    const projectCodesWithUser = await Promise.all(projectCodes.map(async (projectCode) => {
-        const user = await User.findById(projectCode.userId).lean().exec()
-        return { ...projectCode, username: user.username }
-    }))
-
-    res.json(projectCodesWithUser)
-})
+  res.json(projectCodesWithUser);
+});
 
 // @desc Create new projectCode
 // @route POST /projectCodes
 // @access Private
 const createNewProjectCode = asyncHandler(async (req, res) => {
-
-  const {
-    userId,
-    media,
-    year,
-    finalCustomerId,
-    brand,
-    desc,
-    month,
-    code
-  } = req.body
+  const { userId, media, year, finalCustomerId, brand, desc, month, code } =
+    req.body;
 
   try {
     if (code) {
@@ -45,10 +39,10 @@ const createNewProjectCode = asyncHandler(async (req, res) => {
         brand,
         desc,
         month,
-        code
-      })
+        code,
+      });
 
-      await projectCode.save()
+      await projectCode.save();
     } else {
       const projectCode = new ProjectCode({
         userId,
@@ -56,98 +50,88 @@ const createNewProjectCode = asyncHandler(async (req, res) => {
         year,
         finalCustomerId,
         brand,
-        desc
-      })
+        desc,
+      });
 
-      await projectCode.save()
+      await projectCode.save();
     }
 
     res.status(201).json({
-      message: 'Project code created successfully',
-    })
-
+      message: "Project code created successfully",
+    });
   } catch (error) {
-    res.status(400).json({ message: 'BAD REQUEST: Unable to create project code', error: error.message })
+    res.status(400).json({
+      message: "BAD REQUEST: Unable to create project code",
+      error: error.message,
+    });
   }
-})
+});
 
 // @desc Update a finalCustomer
 // @route PATCH /finalCustomers
 // @access Private
 const updateProjectCode = asyncHandler(async (req, res) => {
+  const { id, userId, media, year, finalCustomerId, brand, desc, month } =
+    req.body;
 
-    const {
-        id,
-        userId,
-        media,
-        year,
-        finalCustomerId,
-        brand,
-        desc,
-        month
-    } = req.body
+  if (!userId || !finalCustomerId || !brand || !year)
+    return res.status(400).json({
+      success: false,
+      message: "BAD REQUEST : fields are required",
+    });
 
-    if (!userId || !finalCustomerId || !brand || !year )
-        return res.status(400).json({
-            success: false,
-            message: 'BAD REQUEST : fields are required'
-        })
+  const projectCode = await ProjectCode.findById(id);
 
-    const projectCode = await ProjectCode.findById(id)
+  if (!projectCode)
+    return res.status(400).json({
+      success: false,
+      message: "BAD REQUEST : Project Code not found",
+    });
 
-    if (!projectCode)
-      return res.status(400).json({
-        success: false,
-        message: 'BAD REQUEST : Project Code not found'
-    })
+  const duplicate = await ProjectCode.findOne({ id }).lean().exec();
+  if (duplicate && duplicate._id.toString() !== id) {
+    return res.status(409).json({
+      success: false,
+      message: "CONFLICT :Duplicate Project Code id",
+    });
+  }
 
-    const duplicate = await ProjectCode.findOne({ id }).lean().exec()
-    if (duplicate  && duplicate._id.toString() !== id) {
-            return res.status(409).json({
-                success: false,
-                message: 'CONFLICT :Duplicate Project Code id'
-            })
-    }
+  projectCode.userId = userId;
+  projectCode.media = media;
+  projectCode.year = year;
+  projectCode.finalCustomerId = finalCustomerId;
+  projectCode.brand = brand;
+  projectCode.desc = desc;
+  projectCode.month = month;
 
-    projectCode.userId = userId
-    projectCode.media = media
-    projectCode.year = year
-    projectCode.finalCustomerId = finalCustomerId
-    projectCode.brand = brand
-    projectCode.desc = desc
-    projectCode.month = month
+  await projectCode.save();
 
-    await projectCode.save()
-
-    res.status(200).json({
-        success: true,
-        message: `UPDATED: Project Code ${projectCode.code} updated successfully!`
-    })
-})
+  res.status(200).json({
+    success: true,
+    message: `UPDATED: Project Code ${projectCode.code} updated successfully!`,
+  });
+});
 
 // @desc Delete a projectCode
 // @route DELETE /projectCode
 // @access Private
 const deleteProjectCode = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+  if (!id) return res.status(400).json({ message: "Project Code ID required" });
 
-    const { id } = req.body
-    if (!id)
-        return res.status(400).json({ message: 'Project Code ID required' })
+  const projectCode = await ProjectCode.findById(id).exec();
+  if (!projectCode)
+    return res.status(400).json({ message: "Project Code not found" });
 
-    const projectCode = await ProjectCode.findById(id).exec()
-    if (!projectCode)
-        return res.status(400).json({ message: 'Project Code not found' })
+  const result = await projectCode.deleteOne();
+  const reply = `Project Code '${result.code}' with ID ${result._id} deleted`;
 
-    const result = await projectCode.deleteOne()
-    const reply = `Project Code '${result.code}' with ID ${result._id} deleted`
-
-    res.json(reply)
-})
-
+  res.json(reply);
+});
 
 module.exports = {
-    getAllProjectCodes,
-    createNewProjectCode,
-    updateProjectCode,
-    deleteProjectCode
-}
+  getAllProjectCodes,
+  createNewProjectCode,
+  updateProjectCode,
+  deleteProjectCode,
+};
--- model/ProjectCode.js
+++ model/ProjectCode.js
@@ -1,39 +1,46 @@
-const moment = require('jalali-moment')
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const moment = require("jalali-moment");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const projectCodeCounterSchema = new mongoose.Schema({
   _id: { type: String, required: true },
   sequence_value: { type: Number },
-})
+});
 
-const ProjectCodeCounter = mongoose.model('ProjectCodeCounter', projectCodeCounterSchema)
+const ProjectCodeCounter = mongoose.model(
+  "ProjectCodeCounter",
+  projectCodeCounterSchema
+);
 
 const projectCodeSchema = new Schema(
   {
     count: {
       type: Number,
       unique: false,
-      required: false
+      required: false,
+      default: 1,
     },
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'User',
+      ref: "User",
     },
     media: {
       type: String,
       required: true,
-      enum: ['BB', 'MTR', 'BUS', 'NMV'],
+      enum: ["BB", "MTR", "BUS", "NMV"],
     },
     year: {
       type: Number,
-      default: () => getEquivalentValue(moment(new Date().toISOString().slice(0, 10), 'YYYY-MM-DD').jYear()),
+      default: () =>
+        getEquivalentValue(
+          moment(new Date().toISOString().slice(0, 10), "YYYY-MM-DD").jYear()
+        ),
     },
     finalCustomerId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'FinalCustomer',
+      ref: "FinalCustomer",
     },
     brand: {
       type: String,
@@ -58,73 +65,81 @@ const projectCodeSchema = new Schema(
   {
     timestamps: true,
   }
-)
+);
 
-projectCodeSchema.pre('save', async function (next) {
-  const doc = this
+projectCodeSchema.pre("save", async function (next) {
+  const doc = this;
 
   try {
-    const { media, year, month, code } = this
+    const { media, year, month, code } = this;
 
     if (doc.isNew) {
-      if(!month && !code) {
-        const counter = await ProjectCodeCounter.findById('count').exec()
-        if (!counter) await ProjectCodeCounter.create({ _id: 'count', sequence_value: 200 })
+      if (!month && !code) {
+        const counter = await ProjectCodeCounter.findById("count").exec();
+        if (!counter)
+          await ProjectCodeCounter.create({
+            _id: "count",
+            sequence_value: 200,
+          });
 
         const updatedCounter = await ProjectCodeCounter.findByIdAndUpdate(
-          'count',
+          "count",
           { $inc: { sequence_value: 1 } },
           { new: true }
-        )
-        doc.count = updatedCounter.sequence_value
+        );
+        doc.count = updatedCounter.sequence_value;
       }
 
       if (!doc.code) {
-        const generatedCode = generateProjectCode(media, year, generateCounter(doc.count))
-        doc.code = generatedCode
+        const generatedCode = generateProjectCode(
+          media,
+          year,
+          generateCounter(doc.count)
+        );
+        doc.code = generatedCode;
       }
 
       if (code && month) {
-        const generatedCode = expandProjectCode(code, month)
-        doc.code = generatedCode
-        doc.count = Math.random(new Date().getTime()).toFixed(3)
+        const generatedCode = expandProjectCode(code, month);
+        doc.code = generatedCode;
+        doc.count = Math.random(new Date().getTime()).toFixed(3);
       }
     } else {
-      if (doc.isModified('media') || doc.isModified('year')) {
-        doc.code = generateProjectCode(media, year, doc.count)
+      if (doc.isModified("media") || doc.isModified("year")) {
+        doc.code = generateProjectCode(media, year, doc.count);
       }
     }
 
-    next()
+    next();
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 function getEquivalentValue(persianYear) {
-  if (persianYear < 1400) throw new Error('Invalid Persian year')
-  return (persianYear - 1400) * 100
+  if (persianYear < 1400) throw new Error("Invalid Persian year");
+  return (persianYear - 1400) * 100;
 }
 
-function generateCounter (count) {
-  return count++
+function generateCounter(count) {
+  return count++;
 }
 
 function generateProjectCode(media, year, counter) {
   const mediaCodeMap = {
-    'BB': '10',
-    'MTR': '20',
-    'BUS': '30',
-    'NMV': '40',
-  }
+    BB: "10",
+    MTR: "20",
+    BUS: "30",
+    NMV: "40",
+  };
 
-  const mediaCode = mediaCodeMap[media] || '00'
-  const yearCode = getEquivalentValue(year)
-  return `${media}${mediaCode}${yearCode}${counter}`
+  const mediaCode = mediaCodeMap[media] || "00";
+  const yearCode = getEquivalentValue(year);
+  return `${media}${mediaCode}${yearCode}${counter}`;
 }
 
 function expandProjectCode(code, month) {
-  return `${code}-${month.toString().padStart(2, '0')}`
+  return `${code}-${month.toString().padStart(2, "0")}`;
 }
 
-module.exports = mongoose.model('ProjectCode', projectCodeSchema)
+module.exports = mongoose.model("ProjectCode", projectCodeSchema);
