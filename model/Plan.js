2479b7763c8d859e6bb4e820af058c26a1f95678
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-09-17 12:38:06 +0330

    refactor: change dates logic and multiple them by 1000 and add 2 to days

--- model/Plan.js
+++ model/Plan.js
@@ -141,10 +141,10 @@ const planSchema = new Schema(
 planSchema.virtual("structures.structureDurationDiff").get(function () {
   return this.structures.map((structure) => {
     return (
-      moment(structure.duration.sellEnd).diff(
-        moment(structure.duration.sellStart),
+      moment(structure.duration.sellEnd * 1000).diff(
+        moment(structure.duration.sellStart * 1000),
         "days"
-      ) + 1
+      ) + 2
     );
   });
 });
@@ -157,7 +157,6 @@ planSchema.pre("save", function (next) {
   }, 0);
   doc.totalMonthlyFee = totalMonthlyFee;
 
-  // Calculate diff for each structure (no need for moment.unix)
   doc.structures.forEach((structure, index) => {
     if (
       doc.isNew ||
@@ -165,10 +164,10 @@ planSchema.pre("save", function (next) {
       structure.duration.diff === null
     ) {
       const structureDiff =
-        moment(structure.duration.sellEnd).diff(
-          moment(structure.duration.sellStart),
+        moment(structure.duration.sellEnd * 1000).diff(
+          moment(structure.duration.sellStart * 1000),
           "days"
-        ) + 1;
+        ) + 2;
       doc.structures[index].duration.diff = structureDiff;
     }
   });
@@ -184,7 +183,6 @@ planSchema.pre("save", function (next) {
     }
   });
 
-  // Ensure other calculations use moment directly
   doc.structures.forEach((structure, index) => {
     if (
       structure.isModified("totalPeriodCost") ||
@@ -195,6 +193,7 @@ planSchema.pre("save", function (next) {
       structure.isModified("monthlyFee")
     ) {
       if (doc.mark.name === "regular") {
+        console.log("THIS IS BEING CALLEDDDDDDDDDDDDDD!");
         if (doc.userDefinedMonthlyFeeWithDiscount) {
           if (structure.discountType === "percentage") {
             const totalPeriodCost =
@@ -218,20 +217,69 @@ planSchema.pre("save", function (next) {
         doc.structures[index].totalPeriodCost = totalPeriodCost;
       }
     }
+    if (doc.userDefinedMonthlyFeeWithDiscount) {
+      if (structure.discountType === "percentage") {
+        const monthlyFeeWithDiscount = structure.monthlyFeeWithDiscount;
+        structure.discountFee =
+          100 - (monthlyFeeWithDiscount * 100) / structure.monthlyFee;
+      } else if (structure.discountType === "number") {
+        structure.monthlyFeeWithDiscount =
+          structure.monthlyFee - structure.discountFee;
+      }
+    } else {
+      const totalPeriodCost =
+        (structure.monthlyFeeWithDiscount / 30) * structure.duration.diff;
+      doc.structures[index].totalPeriodCost = totalPeriodCost;
+    }
   });
 
-  // Final diff handling when sellStart or sellEnd are modified
+  doc.structures.forEach((structure, index) => {
+    if (structure.isModified("percentage")) {
+      const percentage = structure.monthlyFee / doc.totalMonthlyFee;
+      doc.structures[index].percentage = percentage;
+    }
+  });
+
   doc.structures.forEach((structure, index) => {
     if (
       structure.isModified("duration.sellStart") ||
       structure.isModified("duration.sellEnd")
     ) {
       const structureDiff =
-        moment(structure.duration.sellEnd).diff(
-          moment(structure.duration.sellStart),
+        moment(structure.duration.sellEnd * 1000).diff(
+          moment(structure.duration.sellStart * 1000),
           "days"
-        ) + 1;
+        ) + 2;
       doc.structures[index].duration.diff = structureDiff;
+    }
+  });
+
+  doc.structures.forEach((structure, index) => {
+    if (doc.totalPackagePrice !== null && doc.totalPackagePrice !== undefined) {
+      const calculatedInPackageFee =
+        ((doc.totalPackagePrice * structure.percentage) /
+          structure.duration.diff) *
+        30;
+      console.log("calculatedInPackageFee", calculatedInPackageFee);
+      doc.structures[index].calculatedInPackageFee = calculatedInPackageFee;
+    }
+  });
+
+  doc.structures.forEach((structure, index) => {
+    if (
+      doc.isNew ||
+      typeof structure.totalPeriodCost === "undefined" ||
+      structure.totalPeriodCost === null
+    ) {
+      if (doc.mark.name === "regular") {
+        const totalPeriodCost =
+          (structure.monthlyFeeWithDiscount / 30) * structure.duration.diff;
+        doc.structures[index].totalPeriodCost = totalPeriodCost;
+      } else {
+        const totalPeriodCost =
+          (structure.calculatedInPackageFee / 30) * structure.duration.diff;
+        doc.structures[index].totalPeriodCost = totalPeriodCost;
+      }
     }
   });
 
