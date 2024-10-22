1ba36333d5907d5f90eeed985b1e501d5e1502b7
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-10-22 09:28:40 +0330

    refactor: structure controller

--- controllers/structureController.js
+++ controllers/structureController.js
@@ -1,112 +1,119 @@
-const Structure = require('../model/Structure')
-const asyncHandler = require('express-async-handler')
-const User = require('../model/User')
-const { updateExpiredStructures } = require('../middleware/updateExpiredStructures')
+const Structure = require("../model/Structure");
+const asyncHandler = require("express-async-handler");
+const User = require("../model/User");
+const {
+  updateExpiredStructures,
+} = require("../middleware/updateExpiredStructures");
 
 // @desc Get all structures
 // @route GET /structures
 // @access Private
 const getAllStructures = asyncHandler(async (req, res) => {
+  // await updateExpiredStructures()
 
-    // await updateExpiredStructures()
+  // const structures = await Structure.find({ name: { $regex: /\d$/ } }).lean();
+  const structures = await Structure.find().lean();
+  if (!structures?.length)
+    return res
+      .status(400)
+      .json({ message: "BAD REQUEST : No structures found" });
 
-    const structures = await Structure.find().lean()
-    if (!structures?.length)
-        return res.status(400).json({ message: 'BAD REQUEST : No structures found' })
+  const structuresWithUser = await Promise.all(
+    structures.map(async (structure) => {
+      const user = await User.findById(structure.userId).lean().exec();
+      return { ...structure, username: user.username };
+    })
+  );
 
-    const structuresWithUser = await Promise.all(structures.map(async (structure) => {
-        const user = await User.findById(structure.userId).lean().exec()
-        return { ...structure, username: user.username }
-    }))
-
-    res.json(structuresWithUser)
-})
+  res.json(structuresWithUser);
+});
 
 // @desc Create new structure
 // @route POST /structures
 // @access Private
 const createNewStructure = asyncHandler(async (req, res) => {
+  const { userId, name, location } = req.body;
 
-    const {
-        userId,
-        name,
-        location
-    } = req.body
+  if (!userId || !name || !location)
+    return res
+      .status(400)
+      .json({ message: "BAD REQUEST : All fields are required" });
 
-    if (!userId || !name || !location )
-        return res.status(400).json({ message: 'BAD REQUEST : All fields are required' })
+  const duplicate =
+    name.startsWith("N") || name.startsWith("B")
+      ? await Structure.findOne({ name }).lean().exec()
+      : null;
+  if (duplicate)
+    return res
+      .status(409)
+      .json({ message: "CONFLICT :Duplicate structure name" });
 
-    const duplicate = await Structure.findOne({ name }).lean().exec()
-    if (duplicate)
-        return res.status(409).json({ message: 'CONFLICT :Duplicate structure name' })
-
-    const structure = await Structure.create({ userId, name, location })
-    if (structure)
-        return res.status(201).json({ message: `CREATED: Structure ${req.body.name} created successfully!` })
-    else
-        return res.status(400).json({ message: 'BAD REQUEST : Invalid box data received' })
-})
+  const structure = await Structure.create({ userId, name, location });
+  if (structure)
+    return res.status(201).json({
+      message: `CREATED: Structure ${req.body.name} created successfully!`,
+    });
+  else
+    return res
+      .status(400)
+      .json({ message: "BAD REQUEST : Invalid box data received" });
+});
 
 // @desc Update a structure
 // @route PATCH /structures
 // @access Private
 const updateStructure = asyncHandler(async (req, res) => {
+  const { id, userId, name, location, isAvailable, isChosen, parent } =
+    req.body;
 
-    const {
-        id,
-        userId,
-        name,
-        location,
-        isAvailable,
-        isChosen,
-        parent
-    } = req.body
+  if (!id || !userId || !name || !location)
+    return res
+      .status(400)
+      .json({ message: "BAD REQUEST : All fields are required" });
 
-    if (!id || !userId || !name || !location )
-        return res.status(400).json({ message: 'BAD REQUEST : All fields are required' })
+  const structure = await Structure.findById(id).exec();
+  if (!structure)
+    res.status(400).json({ message: "BAD REQUEST : Structure not found" });
 
-    const structure = await Structure.findById(id).exec()
-    if(!structure) res.status(400).json({ message: 'BAD REQUEST : Structure not found' })
+  const duplicate = await Structure.findOne({ name }).lean().exec();
+  if (duplicate && duplicate?._id.toString() !== id)
+    return res
+      .status(409)
+      .json({ message: "CONFLICT : Duplicate structure name!" });
 
-    const duplicate = await Structure.findOne({ name }).lean().exec()
-    if(duplicate && duplicate?._id.toString() !== id)
-        return res.status(409).json({ message: 'CONFLICT : Duplicate structure name!' })
+  // Update and store user
+  structure.userId = userId;
+  structure.name = name;
+  structure.location = location;
+  structure.isAvailable = isAvailable;
+  structure.isChosen = isChosen;
+  structure.parent = parent;
 
-    // Update and store user
-    structure.userId = userId
-    structure.name = name
-    structure.location = location
-    structure.isAvailable = isAvailable
-    structure.isChosen = isChosen
-    structure.parent = parent
+  const updatedStructure = await structure.save();
 
-    const updatedStructure = await structure.save()
-
-    res.json(`'${updatedStructure.sysCode}' updated`)
-})
+  res.json(`'${updatedStructure.sysCode}' updated`);
+});
 
 // @desc Delete a structure
 // @route DELETE /structures
 // @access Private
 const deleteStructure = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+  if (!id) return res.status(400).json({ message: "Structure ID required" });
 
-    const { id } = req.body
-    if (!id)
-        return res.status(400).json({ message: 'Structure ID required' })
+  const structure = await Structure.findById(id).exec();
+  if (!structure)
+    return res.status(400).json({ message: "Structure not found" });
 
-    const structure = await Structure.findById(id).exec()
-    if (!structure)
-        return res.status(400).json({ message: 'Structure not found' })
+  const result = await structure.deleteOne();
+  const reply = `Structure '${result.name}' with ID ${result._id} deleted`;
 
-    const result = await structure.deleteOne()
-    const reply = `Structure '${result.name}' with ID ${result._id} deleted`
-
-    res.json(reply)
-})
+  res.json(reply);
+});
 
 module.exports = {
-    getAllStructures,
-    createNewStructure,
-    updateStructure,
-    deleteStructure
-}
+  getAllStructures,
+  createNewStructure,
+  updateStructure,
+  deleteStructure,
+};
--- routes/boxRoutes.js
+++ routes/boxRoutes.js
@@ -1,15 +1,15 @@
-const express = require("express")
-const router = express.Router()
-const boxController =  require('../controllers/boxController')
+const express = require("express");
+const router = express.Router();
+const boxController = require("../controllers/boxController");
 
-router.route('/')
-    .get(boxController.getAllBoxes)
-    .post(boxController.createNewBox)
-    .patch(boxController.updateBox)
-    .delete(boxController.deleteBox)
+router
+  .route("/")
+  .get(boxController.getAllBoxes)
+  .post(boxController.createNewBox)
+  .patch(boxController.updateBox)
+  .delete(boxController.deleteBox);
 
-router.route("/:id")
-  .get(boxController.getBoxById)
+router.route("/:id").get(boxController.getBoxById);
+router.route("/others").post(boxController.createStructureAndUpdateBox);
 
-
-module.exports = router  
+module.exports = router;
